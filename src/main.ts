3a4a0013b49e4c97cf6aed7f30149aa89fd63d62
henohenon <66393463+henohenon@users.noreply.github.com>
2025-05-08 12:12:56 +0900

    feat: text alive init

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-console.log('Heno, world!');
+import { Player as TextAlivePlayer, type IPlayerApp } from "textalive-app-api";
+
+const taPlayer = new TextAlivePlayer({ app: { token: import.meta.env.VITE_TEXT_ALIVE_TOKEN } });
+taPlayer.addListener({
+  onVideoReady: (app: IPlayerApp) => {
+    console.log("App is ready", app);
+  },
+});
---
+++ src/vite-env.d.ts
@@ -0,0 +1,8 @@
+
+interface ImportMetaEnv {
+    readonly VITE_TEXT_ALIVE_TOKEN: string;
+}
+  
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
