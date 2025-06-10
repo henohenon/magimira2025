adc6ae119e5211df23b1d2640e8d97b16bd859ac
henohenon <66393463+henohenon@users.noreply.github.com>
2025-06-10 20:26:10 +0900

    refactor: reorganize dev controls and implement batch accordion closing
    - Moved dev control imports to a new `controls/index.ts` file for better modularity.
    - Added a "close all" button in `index.html` with functionality to close all `dev-accordion` elements.

---
+++ src/dev/controls/index.ts
@@ -0,0 +1,16 @@
+import "./animation.ts";
+import "./camera.ts";
+import "./light.ts";
+import "./counter.ts";
+import "./spectrum.ts";
+import "./ripple.ts";
+
+import type {Accordion} from "../web-components/accordion.ts";
+
+const accordions = document.querySelectorAll("dev-accordion") as NodeListOf<Accordion>;
+const closeAllButton = document.getElementById("close-all-button");
+closeAllButton?.addEventListener("click", () => {
+    for(const accordion of accordions) {
+        accordion.close();
+    }
+});
--- src/dev/index.ts
+++
@@ -1,6 +0,0 @@
-import "./controls/animation.ts";
-import "./controls/camera.ts";
-import "./controls/light.ts";
-import "./controls/counter.ts";
-import "./controls/spectrum.ts";
-import "./controls/ripple.ts";
