0d2f2817de6d71bf19401dbc02003300926d3a92
henohenon <66393463+henohenon@users.noreply.github.com>
2025-06-13 10:47:17 +0900

    fix: cloudflare build error
    update `timer` type in `bottom-lyrics.ts` for improved type safety

--- src/effects/dom/bottom-lyrics.ts
+++ src/effects/dom/bottom-lyrics.ts
@@ -5,7 +5,7 @@ if (!lyrixText) {
     throw new Error("Lyric text not found");
 }
 
-let timer: Timer;
+let timer: ReturnType<typeof setTimeout>;
 events.on("onPhrase", (e) => {
     lyrixText.innerText = e.phrase;
     clearTimeout(timer);
