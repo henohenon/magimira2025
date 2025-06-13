e7f9b4c55a8bc7ef2d2b0a0158426ae2a7e426b2
henohenon <66393463+henohenon@users.noreply.github.com>
2025-06-13 09:55:26 +0900

    feat: add new event handlers for text-alive animations
    - Introduced handlers for `onPhrase`, `onWord`, `onChar`, `onBeat`, `onChord`, `onChorus`, `onVocalAmplitude`, and `onValenceArousal` events.

--- src/text-alive/events.ts
+++ src/text-alive/events.ts
@@ -4,5 +4,13 @@ type Events = {
     onAppReady: undefined;
     onGameStart: undefined;
     onAudioDomReady: HTMLMediaElement;
+    onPhrase: { phrase: string, duration: number };
+    onWord: { word: string, duration: number };
+    onChar: { char: string, duration: number };
+    onBeat: { duration: number, length: number };
+    onChord: { name: string, duration: string };
+    onChorus: { duration: number };
+    onVocalAmplitude: { amplitude: number };
+    onValenceArousal: { valence: number, arousal: number };
 }
 export const events: Emitter<Events> = mitt<Events>();
