93fdfa97d9b49bf60226d757fc211bab8a0e1fba
henohenon <66393463+henohenon@users.noreply.github.com>
2025-06-15 09:00:59 +0900

    refactor: extract input handling logic to `input.ts` and clean up `index.ts`
    - Moved input handling and associated dependencies (`mitt`, `prismatix-input`) to a new `input.ts` module for better modularity.
    - Updated imports in `counter.ts` to reference `input.ts`.
    - Removed redundant input handling logic and unused imports from `index.ts`.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,10 @@
-import mitt from "mitt";
-import { createSubjects } from "prismatix-input/mitt";
-import type {
-    KeyboardInputEvent,
-    KeyboardInputOptions,
-    WithPositionInputEvent,
-    PointerInputOptions
-} from "prismatix-input/web-native";
-import type {
-    CounterInputEvent,
-    DurationInputEvent,
-    RepeatInputEvent,
-    CounterMiddleware
-} from "prismatix-input/middleware";
-import { keyboardInput, pointerInput } from "prismatix-input/web-native";
-import { counterMiddleware, repeatInput, startToEndDurationInput } from "prismatix-input/middleware";
-import type {Subject} from "prismatix-input/subject";
-
 import "./babylon";
 import "./text-alive";
+import "./input.ts";
 import { events as babylonEvents } from "./babylon/events";
 import {events, events as textaliveEvents} from "./text-alive/events";
 import { playAnimation } from "./babylon/mdl";
-import {addFrequency, type Spectrum} from "./effects/spectrum";
+import {type Spectrum} from "./effects/spectrum";
 import { circleSpectrum, verticalSpectrum, horizontalSpectrum } from "./effects/spectrum";
 import {updateEffects} from "./effects";
 import {player, updateTextAlive} from "./text-alive";
@@ -38,13 +21,6 @@ import {
     colorFullAll
 } from "./effects/preset";
 
-type Events = {
-    anyInput: KeyboardInputEvent | WithPositionInputEvent;
-    duration: DurationInputEvent;
-    durationRepeat: DurationInputEvent | RepeatInputEvent;
-    counter: CounterInputEvent;
-};
-
 export const spectrums: Record<string, Spectrum> = {
     "circle": circleSpectrum(),
     "horizontal": horizontalSpectrum(),
@@ -58,17 +34,6 @@ const presets = [
     cameraMoveAndHorizontalSpectrum,
     colorFullAll
 ];
-
-const emitter = mitt<Events>();
-export const { anyInput, duration, durationRepeat, counter } = createSubjects(emitter, ["anyInput", "durationRepeat", "duration", "counter"]);
-
-const inputArea = document.getElementById("input-area");
-pointerInput(anyInput, { events: ["pointerdown", "pointerup"], target: inputArea } as PointerInputOptions);
-keyboardInput(anyInput as Subject<KeyboardInputEvent>, { events: ["keydown-norepeat", "keyup"], target: inputArea as EventTarget } as KeyboardInputOptions);
-startToEndDurationInput(anyInput, [durationRepeat as Subject<DurationInputEvent>, duration], { minDuration: 300 });
-repeatInput(anyInput, durationRepeat as Subject<RepeatInputEvent>)
-export const counterInstance  = counterMiddleware(durationRepeat, counter) as CounterMiddleware;
-
 
 let babylonLoaded = false;
 let textaliveLoaded = false;
@@ -89,20 +54,6 @@ textaliveEvents.on("onGameStart", () => {
     textaliveBannerFadeOut();
 
     playAnimation("startListen");
-
-    anyInput.subscribe(() => {
-        addFrequency(50);
-        counterInstance.set(counterInstance.get() + 1);
-    });
-
-    duration.subscribe(() => {
-        console.log("duration");
-        playAnimation("listening");
-    });
-    counter.subscribe((e) => {
-        console.log("counter", e);
-        // setLightness(100 - e.count/2);
-    });
 
     // Change random preset when a beat is detected
     textaliveEvents.on("onBeat", () => {
---
+++ src/input.ts
@@ -0,0 +1,35 @@
+﻿import mitt from "mitt";
+import {createSubjects} from "prismatix-input/mitt";
+import {
+    keyboardInput,
+    type KeyboardInputEvent, type KeyboardInputOptions,
+    pointerInput,
+    type PointerInputOptions, type WithPositionInputEvent
+} from "prismatix-input/web-native";
+import type {Subject} from "prismatix-input/subject";
+import {
+    type CounterInputEvent,
+    type CounterMiddleware,
+    counterMiddleware,
+    type DurationInputEvent,
+    repeatInput,
+    type RepeatInputEvent,
+    startToEndDurationInput
+} from "prismatix-input/middleware";
+
+type Events = {
+    anyInput: KeyboardInputEvent | WithPositionInputEvent;
+    duration: DurationInputEvent;
+    durationRepeat: DurationInputEvent | RepeatInputEvent;
+    counter: CounterInputEvent;
+};
+
+const emitter = mitt<Events>();
+export const { anyInput, duration, durationRepeat, counter } = createSubjects(emitter, ["anyInput", "durationRepeat", "duration", "counter"]);
+
+const inputArea = document.getElementById("input-area");
+pointerInput(anyInput, { events: ["pointerdown", "pointerup"], target: inputArea } as PointerInputOptions);
+keyboardInput(anyInput as Subject<KeyboardInputEvent>, { events: ["keydown-norepeat", "keyup"], target: inputArea as EventTarget } as KeyboardInputOptions);
+startToEndDurationInput(anyInput, [durationRepeat as Subject<DurationInputEvent>, duration], { minDuration: 300 });
+repeatInput(anyInput, durationRepeat as Subject<RepeatInputEvent>)
+export const counterInstance  = counterMiddleware(durationRepeat, counter) as CounterMiddleware;
