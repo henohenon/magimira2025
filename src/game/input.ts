2dbd68bb79d77e95baf04c4a5679db480b6843db
AliceWonerfulWorld <gomaazarasi1789@gmail.com>
2025-06-18 22:50:04 +0900

    Fix type casting for pointer input and counter instance

--- src/game/input.ts
+++ src/game/input.ts
@@ -11,7 +11,6 @@ import type {
 import type {Subject} from "prismatix-input/subject";
 import {
     type CounterInputEvent,
-    type CounterMiddleware,
     counterMiddleware,
     type DurationInputEvent,
     repeatInput,
@@ -31,8 +30,8 @@ const emitter = mitt<Events>();
 export const { anyInput, pointer, duration, durationRepeat, counter } = createSubjects(emitter, ["anyInput", "pointer", "durationRepeat", "duration", "counter"]);
 
 const inputArea = document.getElementById("input-area");
-pointerInputWithPosition([anyInput as Subject<WithPositionInputEvent>, pointer], { events: ["pointerdown", "pointermove", "pointerup"], target: inputArea } as PointerInputOptions);
+pointerInputWithPosition([anyInput as Subject<WithPositionInputEvent>, pointer], { events: ["pointerdown", "pointermove", "pointerup"], target: inputArea } as unknown as PointerInputOptions);
 keyboardInput(anyInput as Subject<KeyboardInputEvent>, { events: ["keydown-norepeat", "keyup"], target: inputArea as EventTarget } as KeyboardInputOptions);
 startToEndDurationInput(anyInput, [durationRepeat as Subject<DurationInputEvent>, duration], { minDuration: 300 });
 repeatInput(anyInput, durationRepeat as Subject<RepeatInputEvent>)
-export const counterInstance = counterMiddleware(durationRepeat, counter) as CounterMiddleware;
+export const counterInstance = counterMiddleware(durationRepeat, counter) as any;
