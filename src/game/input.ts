bf4deebba564d90bcffe8bbe29c2925d96a1d07e
henohenon <66393463+henohenon@users.noreply.github.com>
2025-06-19 12:49:22 +0900

    refactor: enhance type safety and fix incorrect type casting in `input.ts`

--- src/game/input.ts
+++ src/game/input.ts
@@ -10,7 +10,7 @@ import type {
 } from "prismatix-input/web-native";
 import type {Subject} from "prismatix-input/subject";
 import {
-    type CounterInputEvent,
+    type CounterInputEvent, type CounterMiddleware,
     counterMiddleware,
     type DurationInputEvent,
     repeatInput,
@@ -30,8 +30,8 @@ const emitter = mitt<Events>();
 export const { anyInput, pointer, duration, durationRepeat, counter } = createSubjects(emitter, ["anyInput", "pointer", "durationRepeat", "duration", "counter"]);
 
 const inputArea = document.getElementById("input-area");
-pointerInputWithPosition([anyInput as Subject<WithPositionInputEvent>, pointer], { events: ["pointerdown", "pointermove", "pointerup"], target: inputArea } as unknown as PointerInputOptions);
+pointerInputWithPosition([anyInput as Subject<WithPositionInputEvent>, pointer], { events: ["pointerdown", "pointermove", "pointerup"], target: inputArea } as PointerInputOptions);
 keyboardInput(anyInput as Subject<KeyboardInputEvent>, { events: ["keydown-norepeat", "keyup"], target: inputArea as EventTarget } as KeyboardInputOptions);
 startToEndDurationInput(anyInput, [durationRepeat as Subject<DurationInputEvent>, duration], { minDuration: 300 });
 repeatInput(anyInput, durationRepeat as Subject<RepeatInputEvent>)
-export const counterInstance = counterMiddleware(durationRepeat, counter) as any;
+export const counterInstance = counterMiddleware(durationRepeat, counter) as CounterMiddleware;
